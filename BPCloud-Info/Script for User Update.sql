d8ddc7edc3d211854edfda6c9856e14f464dcac6
pramoddp <dppramod1@gmail.com>
2022-03-10 09:14:06 +0530

    pushing

---
+++ BPCloud-Info/Script for User Update.sql
@@ -0,0 +1,5 @@
+use BPCloudAuthentication
+select * from Users order by 1 desc
+
+update Users set Email = 'iteosin@gmail.com' , [UserName]='Chand'  where UserID='702CC5DE-24F8-4BF5-896A-1A7DF466D7A5'
+--EKrT47/XLhJ1O120BKbYzQ== password
